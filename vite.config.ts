95a89f1e9a0596faed83cd78f6775619a5ff7e27
enjo1ns <discord.pakistan55@gmail.com>
2025-06-10 22:47:43 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/your-repository-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'pookie.github.io' : '/',
   server: {
     host: "::",
     port: 8080,
